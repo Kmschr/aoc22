af04197fcc7398f3f512d591800ac7593051ff52
Kevin Schroeder <kmschr@gmail.com>
2022-12-01 18:09:30 -0600

    clean code

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -3,18 +3,22 @@ use itertools::*;
 const DATA: &str = include_str!("input");
 
 pub fn part1() -> i32 {
-    DATA.lines()
-        .group_by(|line: &&str| line != &"")
+    count_calories()
         .into_iter()
-        .map(|(is_calorie, calories_list)| {
-            if !is_calorie {
-                return 0;
-            }
-            calories_list.map(|calories: &str| str::parse::<i32>(calories).unwrap()).sum()
-        }).max().expect("failed to get max calories")
+        .max()
+        .expect("failed to get max calories")
 }
 
 pub fn part2() -> i32 {
+    count_calories()
+        .into_iter()
+        .sorted()
+        .rev()
+        .take(3)
+        .sum()
+}
+
+pub fn count_calories() -> Vec<i32> {
     DATA.lines()
         .group_by(|line: &&str| line != &"")
         .into_iter()
@@ -23,5 +27,5 @@ pub fn part2() -> i32 {
                 return 0;
             }
             calories_list.map(|calories: &str| str::parse::<i32>(calories).unwrap()).sum()
-        }).sorted().rev().take(3).sum()
+        }).collect()
 }
